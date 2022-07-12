97eef855d1d1bb9cad8332309ac242beafdbb38e
Robyn Wang <robyn@productplan.com>
2022-07-11 19:37:00 -0700

    puppeteer screenshots pdf

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
   def index
+    system("node app/javascript/puppeteerScreenshot.js #{Shellwords.escape("https://google.com")} #{Shellwords.escape("output.pdf")}")
   end
 end
